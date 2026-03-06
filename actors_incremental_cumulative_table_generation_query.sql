3c7446951babb410535440a2d5ac197c100d176b
Nathaniel Hwan Choi <nathaniel.choi123@gmail.com>
2026-03-06 16:12:36 -0500

    Update actors_incremental_cumulative_table_generation_query.sql

--- actors_incremental_cumulative_table_generation_query.sql
+++ actors_incremental_cumulative_table_generation_query.sql
@@ -9,35 +9,37 @@ WITH params AS (
 ),
 
 last_year AS (
-	SELECT *
-	FROM actors, params
-	WHERE current_year = params.last_year
+	SELECT
+		a.*
+	FROM actors a,
+	JOIN params p
+	WHERE a.current_year = p.last_year
 ),
 
 current_year AS (
     SELECT 
-        actorid,
-        actor,
+        af.actorid,
+        af.actor,
         ARRAY_AGG(
             ROW(
-                film,
-                votes,
-                rating,
-                filmid
+                af.film,
+                af.votes,
+                af.rating,
+                af.filmid
             )::films
             ORDER BY filmid
         ) AS films,
         AVG(rating) AS avg_rating
-    FROM actor_films, params
-    WHERE year = params.current_year
-    GROUP BY actorid, actor
+    FROM actor_films af 
+	JOIN params p
+	ON af.year = p.current_year
+    GROUP BY af.actorid, af.actor
 ),
 
 cumulative AS (
 SELECT 
 	COALESCE(ly.actor, cy.actor) AS actor,
 	COALESCE(ly.actorid, cy.actorid) AS actorid,
-
 	CASE
 		WHEN ly.films IS NULL THEN cy.films
 		WHEN cy.films IS NULL THEN ly.films
@@ -54,17 +56,21 @@ SELECT
 			END::quality_class
 		ELSE ly.quality_class
 	END AS quality_class,
-
 	cy.actorid IS NOT NULL AS is_active,
-
-	params.current_year AS current_year
-
-FROM last_year ly
+	p.current_year AS current_year
+FROM last_year ly 
 FULL OUTER JOIN current_year cy
 	ON ly.actorid = cy.actorid
 CROSS JOIN params
 )
 
 INSERT INTO actors
-SELECT *
+(actor, actorid, films, quality_class, is_active, current_year)
+SELECT 
+	actor, 
+	actorid,
+	films,
+	quality_class,
+	is_active,
+	current_year
 FROM cumulative;
