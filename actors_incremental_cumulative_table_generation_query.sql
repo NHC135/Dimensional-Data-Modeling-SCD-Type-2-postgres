edf9e20a019628d0f5e7f07fbc57b03001f7df8e
Nathaniel Hwan Choi <nathaniel.choi123@gmail.com>
2026-03-06 15:17:37 -0500

    Update actors_incremental_cumulative_table_generation_query.sql
    added "actorid" for more reliable transformations

--- actors_incremental_cumulative_table_generation_query.sql
+++ actors_incremental_cumulative_table_generation_query.sql
@@ -2,17 +2,20 @@
 incremental cumulative table generation query
 ===========================================*/
 
+WITH params AS (
+    SELECT
+        2021 AS last_year,
+        2022 AS current_year
+),
 
--- for incremental we establish a last year & current year
-WITH last_year AS (
-	SELECT 
-	*
-	FROM actors
-	WHERE current_year = 2021 -- Change this for each year 1970 -2021
-), 
+last_year AS (
+	SELECT *
+	FROM actors, params
+	WHERE current_year = params.last_year
+),
 
 current_year AS (
- SELECT 
+    SELECT 
         actorid,
         actor,
         ARRAY_AGG(
@@ -22,39 +25,46 @@ current_year AS (
                 rating,
                 filmid
             )::films
+            ORDER BY filmid
         ) AS films,
         AVG(rating) AS avg_rating
-    FROM actor_films
-    WHERE year = 2022 -- Change this for each year (e.g., 1970 - 2021)
+    FROM actor_films, params
+    WHERE year = params.current_year
     GROUP BY actorid, actor
 ),
 
 cumulative AS (
 SELECT 
-	COALESCE(ly.actor, cy.actor) AS actor, 
-	COALESCE(ly.actorid, cy.actorid) AS actorid, 
+	COALESCE(ly.actor, cy.actor) AS actor,
+	COALESCE(ly.actorid, cy.actorid) AS actorid,
+
 	CASE
-		WHEN ly.films IS NULL THEN cy.films 
-		WHEN cy.films IS NOT NULL THEN ly.films
+		WHEN ly.films IS NULL THEN cy.films
+		WHEN cy.films IS NULL THEN ly.films
 		ELSE ly.films || cy.films
 	END AS films,
-	CASE WHEN cy.avg_rating IS NOT NULL THEN 
-		(CASE 
-			WHEN cy.avg_rating > 8 THEN 'star'
-			WHEN cy.avg_rating > 7 THEN 'good'
-			WHEN cy.avg_rating > 6 THEN 'average'
-			ELSE 'bad'
-		END)::quality_class
-		ELSE ly.quality_class 
-	END AS quality_class, 
+
+	CASE 
+		WHEN cy.avg_rating IS NOT NULL THEN
+			CASE 
+				WHEN cy.avg_rating > 8 THEN 'star'
+				WHEN cy.avg_rating > 7 THEN 'good'
+				WHEN cy.avg_rating > 6 THEN 'average'
+				ELSE 'bad'
+			END::quality_class
+		ELSE ly.quality_class
+	END AS quality_class,
+
 	cy.actorid IS NOT NULL AS is_active,
-	2022 AS current_year
+
+	params.current_year AS current_year
+
 FROM last_year ly
-FULL OUTER JOIN current_year cy 
-ON ly.actorid = cy.actorid 
+FULL OUTER JOIN current_year cy
+	ON ly.actorid = cy.actorid
+CROSS JOIN params
 )
 
 INSERT INTO actors
-SELECT
-*
-FROM cumulative
+SELECT *
+FROM cumulative;
