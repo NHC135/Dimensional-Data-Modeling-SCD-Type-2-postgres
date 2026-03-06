87fbaf5eac062d283a927307acdc7a27d904a5c0
Nathaniel Hwan Choi <nathaniel.choi123@gmail.com>
2026-03-06 14:39:43 -0500

    Update actors-history-scd.sql
    Added safeguards to make for robustness

--- DDL/actors-history-scd.sql
+++ DDL/actors-history-scd.sql
@@ -2,13 +2,14 @@
 DDL actors_history_scd table
 =============================*/
 
-CREATE TABLE actors_history_scd (
-	actorid TEXT,
-	actor TEXT,
-	quality_class quality_class,
-	is_active BOOLEAN,
-	start_year INTEGER, 
-	end_year INTEGER,
-	current_year INTEGER,
+CREATE TABLE IF NOT EXISTS actors_history_scd (
+	actorid TEXT NOT NULL,
+	actor TEXT NOT NULL,
+	quality_class quality_class NOT NULL,
+	is_active BOOLEAN NOT NULL,
+	start_year INTEGER NOT NULL, 
+	end_year INTEGER NOT NULL,
+	current_year INTEGER NOT NULL,
 	PRIMARY KEY(actorid, start_year, end_year)
-);
+); 
+
